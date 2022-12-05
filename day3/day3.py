9ef24eefa51fe1e33cd7367c9a2e0d8787380569
justinschuster <schujustin@gmail.com>
2022-12-05 17:31:40 -0500

    Completed day 3 part two

--- day3/day3.py
+++ day3/day3.py
@@ -1,9 +1,3 @@
-# each rucksack has two compartments
-# failed packing for 1 item per rucksack
-# find errors
-# A != a
-# first half of items = carpartment 1, etc
-
 def get_input():
     with open('day3/day3_input') as file:
         return file.readlines()
@@ -14,21 +8,18 @@ def halve_string(s):
     string2 = s[len(s)//2:]
     return string1, string2
 
-def get_priority_lower(c):
-    return ord(c) - 96
-
-def get_priority_upper(c):
-    return ord(c) - 38
-
 def compare(string1, string2):
     for c in string1:
             for d in string2:
-                print('{} {}'.format(c, d))
                 if c == d:
-                    if c.isupper():
-                        return get_priority_upper(c)
-                    else:
-                        return get_priority_lower(c)
+                    return get_value(c)
+
+def get_value(c):
+    if c.isupper():
+        return ord(c) - 38
+    else:
+        return ord(c) - 96
+
 # Part 1
 def both_compartments(lines):
     score = 0
@@ -37,8 +28,22 @@ def both_compartments(lines):
         score += compare(string1, string2)
     return score
 
+def badges(lines):
+    score = 0
+    for i in range(0, len(lines), 3):
+        line1 = lines[i].replace('\n', '')
+        line2 = lines[i+1].replace('\n', '')
+        line3 = lines[i+2].replace('\n', '')
+        common = set(line1).intersection(line2).intersection(line3)
+        score += get_value(list(common)[0])
+    return score
+
 lines = get_input()
 
-# Part 1
+# Part One
 score = both_compartments(lines)
 print('Part One: {}'.format(score))
+
+# Part Two
+score = badges(lines)
+print('Part Two: {}'.format(score))
