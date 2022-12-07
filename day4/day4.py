89ef22fcff033068ad9382bf3dfc20209ffb9c9a
justinschuster <schujustin@gmail.com>
2022-12-07 15:14:13 -0500

    Completed day 4 part 2

--- day4/day4.py
+++ day4/day4.py
@@ -1,12 +1,10 @@
 # Day 4 Advent of Code
 
-schedules = []
-
 def get_input():
     with open('day4/day4_input') as file:
         return file.readlines()
 
-def read_schedules(lines):
+def find_complete_overlap(lines):
     num_contained = 0
     for line in lines:
         schedules = [i.split('-') for i in line.strip().split(',')]
@@ -16,5 +14,24 @@ def read_schedules(lines):
             num_contained += 1
     return num_contained
 
+def find_any_overlap(lines):
+    num_contained = 0
+    for line in lines:
+        schedules = [i.split('-') for i in line.strip().split(',')]
+        if int(schedules[0][0]) <= int(schedules[1][0]) and int(schedules[0][1]) >= int(schedules[1][0]):
+            num_contained += 1
+        elif int(schedules[0][0]) <= int(schedules[1][1]) and int(schedules[0][1]) >= int(schedules[1][1]):
+            num_contained += 1
+        elif int(schedules[1][0]) <= int(schedules[0][0]) and int(schedules[1][1]) >= int(schedules[0][0]):
+            num_contained += 1
+        elif int(schedules[1][0]) <= int(schedules[0][1]) and int(schedules[1][1]) >= int(schedules[0][1]):
+            num_contained += 1
+    return num_contained
+
 lines = get_input()
-print(read_schedules(lines))
+
+# Part One
+print('Part One: {}'.format(find_complete_overlap(lines)))
+
+# Part Two
+print('Part Two: {}'.format(find_any_overlap(lines)))
